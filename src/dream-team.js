04e52912bb6da84fe6147babdf870346446ba8c3
Yauheni Ramankou <iromankov2010@gmail.com>
2020-03-20 23:22:49 +0300

    feat: implement dream-team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,10 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  console.log(Array.isArray(members));
+  if (!Array.isArray(members)) {
+    return false;
+  } else {
+    return members.map(function (member) {
+      return typeof(member) === "string" ? member.replace(/\s+/g, "").substring(0, 1).toUpperCase() : "";
+    }).sort().join("");
+  }
 };
